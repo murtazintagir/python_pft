708d25862596345bd42c443075243577f2fcc6b3
murtazintagir <tr.murtazin@yandex.ru>
2021-06-16 18:12:03 +0300

    Создан проект в PyCharm

--- contact.py
+++ contact.py
@@ -1,7 +1,27 @@
-class Contact:
+class Data:
 
-    def __init__(self, first_name, middle_name, last_name, nickname):
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
         self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+
+class Contact:
+
+    def __init__(self, home, mobile, work, fax, email, email2, email3, homepage, address2, phone2, notes):#, birthday, anniversary):
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        #self.birthday = birthday
+        #self.anniversary = anniversary
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
-from contact import Contact
+from contact import Data, Contact
 
 
 def is_alert_present(wd):
@@ -22,23 +22,59 @@ class test_add_address_book(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_address_book_page(wd)
-        self.create_address_book(wd, Contact(first_name="first", middle_name="middle", last_name="last", nickname="nick"))
+        self.create_data_info(wd, Data(first_name="first", middle_name="middle", last_name="last", nickname="nick", title="title", company="company", address="address"))
+        self.create_contact_info(wd, Contact(home="home", mobile="mobile", work="work", fax="fax", email="email", email2="email2", email3="email3", homepage="homepage", address2="address2", phone2="phone2", notes="notes"))
         self.logout(wd)
 
     def logout(self, wd):
         # logout
         wd.find_element_by_link_text("Logout").click()
 
-    def create_address_book(self, wd, Contact):
+    def create_data_info(self, wd, Data):
         # fill address book
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Contact.first_name)
+        wd.find_element_by_name("firstname").send_keys(Data.first_name)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(Contact.middle_name)
+        wd.find_element_by_name("middlename").send_keys(Data.middle_name)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Contact.last_name)
+        wd.find_element_by_name("lastname").send_keys(Data.last_name)
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(Contact.nickname)
+        wd.find_element_by_name("nickname").send_keys(Data.nickname)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(Data.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(Data.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(Data.address)
+        # submit group creation
+        #wd.find_element_by_name("submit").click()
+
+    def create_contact_info(self, wd, Contact):
+        # fill address book
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(Contact.home)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(Contact.mobile)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(Contact.work)
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(Contact.fax)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(Contact.email)
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(Contact.email2)
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(Contact.email3)
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(Contact.homepage)
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(Contact.address2)
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(Contact.phone2)
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(Contact.notes)
+        #wd.find_element_by_name("bday").click()
+        #wd.find_element_by_name("bday").select_by_visible_text(Contact.birthday)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
