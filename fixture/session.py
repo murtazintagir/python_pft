e8fd6ec3a3e9de7d3c36dc661f4acf88a6c1257e
79203 <tr.murtazin@yandex.ru>
2021-07-04 18:23:07 +0300

    Добавлены дополнительные тесты

--- fixture/session.py
+++ fixture/session.py
@@ -1,20 +1,42 @@
 class SessionHelper:
 
-    def __init__(self,app):
+    def __init__(self, app):
         self.app = app
 
     def login(self, username, password):
-            wd = self.app.wd
-            self.app.open_home_page()
-            # login
-            wd.find_element_by_name("user").clear()
-            wd.find_element_by_name("user").send_keys(username)
-            wd.find_element_by_name("pass").clear()
-            wd.find_element_by_name("pass").send_keys(password)
-            wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd = self.app.wd
+        self.app.open_home_page()
+        # login
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def logout(self):
-            wd = self.app.wd
-            # logout
-            wd.find_element_by_link_text("Logout").click()
-            wd.find_element_by_name("user")
+        wd = self.app.wd
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user")
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
