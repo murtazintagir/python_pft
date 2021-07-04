ef591ed6b464a3f9773141d465a1de375c9639e7
79203 <tr.murtazin@yandex.ru>
2021-07-04 18:24:58 +0300

    Добавлены дополнительные тесты

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="name", header="header", footer="footer"))
-    app.session.logout()
+
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
