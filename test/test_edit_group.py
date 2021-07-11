7aba80c30f6f88a0fb0c35df03bb9bc27bbfd5b7
murtazintagir <tr.murtazin@yandex.ru>
2021-07-11 14:44:36 +0300

    Описание изменений в файле README.md

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,8 +5,11 @@ from model.group import Group
 def test_edit_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    # редактируем параметры для группы
     old_groups = app.group.get_group_list()
-    app.group.edit_group(Group(name="name_edit"))
+    group = Group(name="name_edit")
+    group.id = old_groups[0].id
+    app.group.edit_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
