ab3ca8f592aff1960a7c84083a46a3085c7c1d0e
murtazintagir <tr.murtazin@yandex.ru>
2021-08-01 14:59:37 +0300

    Добавлены дополнительные тесты

--- conftest.py
+++ conftest.py
@@ -7,8 +7,10 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseURL")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
             fixture = Application()
@@ -23,3 +25,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseURL", action="store", default="http://localhost/addressbook/")
