25fe08d251466ceb40710c227fc23c4d6fe6d0b3
murtazintagir <tr.murtazin@yandex.ru>
2021-08-01 18:30:20 +0300

    Реализованы генераторы тестовых данных

--- generator/contact.py
+++ generator/contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -52,4 +52,5 @@ testdata = [Contact(first_name=random_string("first_name", 10), middle_name=rand
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
