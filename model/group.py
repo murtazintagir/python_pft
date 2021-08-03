34559d01700f6c3d40509d60f7751025a447ece1
murtazintagir <tr.murtazin@yandex.ru>
2021-08-03 07:07:16 +0300

    Доработана проверка полей email для сценария сравнения полей в окне редактирования и на главной странице

--- model/group.py
+++ model/group.py
@@ -14,9 +14,7 @@ class Group:
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) \
-               and (self.name == other.name or self.name is None or other.name is None) \
-               and (self.header == other.header or self.header is None or other.header is None) \
-               and (self.footer == other.footer or self.footer is None or other.footer is None)
+               and (self.name == other.name or self.name is None or other.name is None)
 
     def id_or_max(self):
         if self.id:
